4d99f0ce5ec98572b4935227a2ba03cbf879ccd0
RakeshThakur26 <rakesh.thakur.mail99@gmail.com>
2021-09-14 14:19:57 +0530

    Task 1

---
+++ MainClass/MainClass/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n,regno;
+            
+
+            Console.WriteLine("Course details : ");
+            Console.Write("Enter number of courses : ");
+
+            n = Convert.ToInt32(Console.ReadLine());
+            FileHandling obj = new FileHandling();
+            
+
+                obj.WriteFile(n);
+            Console.Read();
+
+
+
+        }
+    }
+}
