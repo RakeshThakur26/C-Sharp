ce58f92f5bfc303777c7cae9bd8c723ed62a6eec
RakeshThakur26 <rakesh.thakur.mail99@gmail.com>
2021-09-15 12:03:28 +0530

    User Defined Class Library

---
+++ Training Day 3/MyClassLibrary/Mobile.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyClassLibrary
+{
+    public class Mobile
+    {
+        public void MobileTypes()
+        {
+            //Types of mobiles OS
+            
+            Console.WriteLine("Types of Mobile : ");
+            Console.WriteLine("Android ");
+            Console.WriteLine("IOS ");
+            Console.WriteLine("Windows ");
+        }
+        public void Model(string inp)
+        {
+            if(inp == "Android")
+                Console.WriteLine("POCO  ");
+            else if(inp == "IOS")
+                 Console.WriteLine("Iphone ");
+            else if(inp == "Windows")
+                Console.WriteLine("Nokia ");
+        }
+    }
+}
